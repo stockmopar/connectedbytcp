02919d337df4957358a24ce48de0daae7fd85635
kevincoleman <kevin.n.coleman@gmail.com>
2015-01-26 23:14:14 -0800

    updated time to longer

--- sunrise.js
+++ sunrise.js
@@ -4,8 +4,8 @@ Sunrise = new TCPConnected("10.0.1.3");
 
 //user vars
 var room = 'Bedroom';
-var fadeDuration = 1;
-var step = 4;
+var fadeDuration = 30;
+var step = 1;
 var sunrise;
 
 // script
@@ -14,7 +14,7 @@ var updateFadeLevel = function() {
   	Sunrise.GetRoomStateByName(room, function(error,state,level){
   		if(state == 0){
   			Sunrise.TurnOnRoomWithLevelByName(room, 1, function(){
-  				sunrise = setTimeout(updateFadeLevel, (fadeDuration * 60));
+  				sunrise = setTimeout(updateFadeLevel, (fadeDuration * 600));
   			});
   		}else{
   			if (level >= 100) {
@@ -23,7 +23,7 @@ var updateFadeLevel = function() {
   			  Sunrise.GWEnd();
   			} else {
   			  Sunrise.SetRoomLevelByName(room, Math.min(100,level+step), function(){
-  				sunrise = setTimeout(updateFadeLevel, (fadeDuration * 60));
+  				sunrise = setTimeout(updateFadeLevel, (fadeDuration * 600));
   			  } );
   			}
   		}
@@ -35,7 +35,7 @@ var updateFadeLevel = function() {
 Sunrise.Init(function(error){
 	if(!error){
 		sunrise = setTimeout(updateFadeLevel, (fadeDuration * 60));
-	}else{
+	}else{c
 		console.log("There was an issue initializing the token");
 	}
 });
