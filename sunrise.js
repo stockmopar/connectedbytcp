90da19801a0da4fb45bdf28f78f3d6ba504f74ac
kevincoleman <kevin.n.coleman@gmail.com>
2015-02-02 00:07:08 -0800

    created fadeIncrement to handle math in one place; reduced step to 1% for smoother sunrise; added minutesOfSunrise for easier user input (in minutes)

--- sunrise.js
+++ sunrise.js
@@ -4,8 +4,9 @@ Sunrise = new TCPConnected("10.0.1.3");
 
 //user vars
 var room = 'Bedroom';
-var fadeDuration = 30;
-var step = 4;
+var minutesOfSunrise = 30; // set your duration here, in human minutes.
+var fadeIncrement = minutesOfSunrise * 600;
+var step = 1;
 var sunrise;
 
 // script
@@ -14,7 +15,7 @@ var updateFadeLevel = function() {
   	Sunrise.GetRoomStateByName(room, function(error,state,level){
   		if(state == 0){
   			Sunrise.TurnOnRoomWithLevelByName(room, 1, function(){
-  				sunrise = setTimeout(updateFadeLevel, (fadeDuration * 60));
+  				sunrise = setTimeout(updateFadeLevel, (fadeIncrement));
   			});
   		}else{
   			if (level >= 100) {
@@ -23,7 +24,7 @@ var updateFadeLevel = function() {
   			  Sunrise.GWEnd();
   			} else {
   			  Sunrise.SetRoomLevelByName(room, Math.min(100,level+step), function(){
-  				sunrise = setTimeout(updateFadeLevel, (fadeDuration * 60));
+  				sunrise = setTimeout(updateFadeLevel, (fadeIncrement));
   			  } );
   			}
   		}
@@ -34,7 +35,7 @@ var updateFadeLevel = function() {
 // run the script
 Sunrise.Init(function(error){
 	if(!error){
-		sunrise = setTimeout(updateFadeLevel, (fadeDuration * 60));
+		sunrise = setTimeout(updateFadeLevel, fadeIncrement);
 	}else{
 		console.log("There was an issue initializing the token");
 	}
